fc8b25e98822a3e03582eeb68284c2baec0d7ef5
[silencebo@163.com] <silencebo@163.com>
2018-06-16 18:03:37 +0800

    客户管理分页版

---
+++ MyCustomerPage/README.md
@@ -0,0 +1,20 @@
+# MyCustomerPage
+一个简单的实现客户增删改查且带有分页功能的程序
+## 主要功能
+- 添加客户
+- 修改客户
+- 查找客户
+- 删除客户
+- 高级搜索
+- 分页功能
+
+## 需要的jar包：
+- myEclipse的JavaEE支持
+- mysql-connector-java-5.1.13-bin.jar
+- commons-beanutils-1.7.0.jar
+- commons-logging-1.2.jar
+- commons-dbutils-1.4.jar
+- c3p0-0.9.2-pre1.jar
+- c3p0-oracle-thin-extras-0.9.2-pre1.jar
+- mchange-commons-0.2.jar
+- itcast-tools-1.4.2.jar(自己封装的jar，已上传，需修改为.jar结尾)
---
+++ MyCustomerPage/src/cn/itcast/cstm/dao/CustomerTest.java
@@ -0,0 +1,28 @@
+package cn.itcast.cstm.dao;
+
+import cn.itcast.commons.CommonUtils;
+import cn.itcast.cstm.domain.Customer;
+
+public class CustomerTest {
+	
+	public static void main(String[] args) {
+		fun1();
+	}
+	
+	public static void fun1() {
+		CustomerDao dao = new CustomerDao();
+		for(int i = 1; i <= 300; i++) {
+			Customer c = new Customer();
+			
+			c.setCid(CommonUtils.uuid());
+			c.setCname("cstm_" + i);
+			c.setBirthday("2014-07-13");
+			c.setGender(i%2==0?"男":"女");
+			c.setCellphone("139" + i);
+			c.setEmail("cstm_" + i + "@163.com");
+			c.setDescription("我是客户");
+			
+			dao.add(c);
+		}
+	}
+}
